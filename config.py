9e5c5bae31d6ed9c5bbf56c185190474d4751fa4
EverseenMovies <therideepak3773@gmail.com>
2024-05-28 06:24:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://filesesm:filesesm@cluster
 DB_NAME = os.environ.get("DATABASE_NAME", "filesesm")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", -1002234703146))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002038756910"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
